37eaf4d9317a508628fbe3fbd2b290b2d76cd44d
Mykhailo Sokyrko <sokirko.mm@gmail.com>
2022-04-13 21:41:16 +0300

    Зависимости + Разметка
    Установка зависимостей : еслинт + емоушн.
    Создан компонент разметки без стилизации и функционала.

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,20 @@
+export const Feedback = () => {
+  return (
+    <div>
+      <h2>Please leave feedback</h2>
+      <div>
+        <button>:)</button>
+        <button>:|</button>
+        <button>:(</button>
+      </div>
+      <h2>Statistics</h2>
+      <div>
+        <span>Good {}</span>
+        <span>Neutral {}</span>
+        <span>Bad {}</span>
+        <span>Total {}</span>
+        <span>Positive feedback {}</span>
+      </div>
+    </div>
+  );
+};
